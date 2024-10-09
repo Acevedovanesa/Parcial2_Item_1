966ae4b9076c59be3e64f8eea597966679fc1a43
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-10-08 21:26:26 -0500

    Inicio codigo

---
+++ Parcial2_Item_1/Parcial2_Item_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool JugarNuevo = true;
+
+        while (JugarNuevo)
+        {
+            // 1. Pedir cantidad de jugadores
+            int NumJugadores = 0;
+            while (NumJugadores < 2 || NumJugadores > 4)
+            {
+                Console.WriteLine("Bienvenidos al juego, Adivina el número! ");
+                Console.WriteLine("Ingrese el número de jugadores (mínimo 2, máximo 4): ");
+                NumJugadores = int.Parse(Console.ReadLine());
+            }
+
+
+
+        }
+    }
+}
