8ac45e69a2ca9e3062529637ced660734ae0667b
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-10-08 22:08:29 -0500

    Inicio item 2

---
+++ Parcial2_Item_2/Parcial2_Item_2/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        {
+            // Inicialización del código, inicialmente se declaran 4 variables con el fin de validar cantidad de estudiantes
+            bool Evaluar = true;
+
+            while (Evaluar)
+            {
+                int NumEstudiantes = 0;
+                int EstGanaron = 0;
+                int EstPerdieron = 0;
+
+                Console.WriteLine("Ingrese la cantidad de estudiantes a evaluar:");
+                NumEstudiantes = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
